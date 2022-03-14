91585722fbc448eecc1be3ccf90f3354670f6a4c
mohamed gamal <mohamed11gamal96@gmail.com>
2022-03-14 15:11:32 +0200

    Implement loading

--- src/main/kotlin/pokemoncreator/ui/UIComponent.kt
+++ src/main/kotlin/pokemoncreator/ui/UIComponent.kt
@@ -1,5 +1,6 @@
 package pokemoncreator.ui
 
+import kotlinx.coroutines.*
 import pokemoncreator.data.repo.PokemonRepoImpl
 import pokemoncreator.domain.usecase.CreatePokemonUseCase
 import pokemoncreator.interfaceadapters.controller.PokemonColors
@@ -26,6 +27,30 @@ class UIComponent: PokemonView {
     // adapter
     private val controller = PokemonCreatorController(createPokemonUseCase)
 
+    //region Do not open!
+    private val uiScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    private var loadingJob : Job? = null
+    private fun showLoading() {
+        loadingJob?.cancel()
+        loadingJob = uiScope.launch {
+            val fullString = "Loading........"
+            var index = 0
+            while (true) {
+                ensureActive()
+                delay(200L)
+                index++
+                val mIndex = index % fullString.length.minus(1)
+                val stringEdge = if (mIndex < 7) mIndex.plus(7) else mIndex
+                print(fullString.substring(0, stringEdge).plus("\r"))
+            }
+        }
+    }
+    private fun stopLoading() {
+        loadingJob?.cancel()
+        loadingJob = null
+    }
+    //endregion
+
     init {
         println("Welcome to Pokemon creator!")
 
@@ -48,7 +73,7 @@ class UIComponent: PokemonView {
         """.trimIndent())
         val color = readlnOrNull() ?: ""
 
-        println("LOADING.....")
+        showLoading()
 
         controller.createPokemon(
             name ?: "No name",
@@ -61,10 +86,16 @@ class UIComponent: PokemonView {
 
 
     override fun onPokemonCreated(pokemonUIModel: PokemonUIModel) {
-        println("Pokemon created as $pokemonUIModel")
+        stopLoading()
+        println("Pokemon created successfully!!")
+        println("Pokemon is: $pokemonUIModel")
     }
 
     override fun onPokemonCreationFailed(errorMessage: String) {
-        println("Pokemon creation failed :( :( with message $errorMessage")
+        stopLoading()
+        println("""
+            Pokemon creation failed :( :( 
+            Message: $errorMessage
+        """.trimIndent())
     }
 }
